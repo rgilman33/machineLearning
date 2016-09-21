fe25d07a01a159ccb0f929a5396b5394c7ed4dcc
rgilman33 <rudygilman@gmail.com>
2016-09-21 10:16:48 -0600

    fdsfsadsa

--- AWScifar10Classifier.py
+++ AWScifar10Classifier.py
@@ -1,15 +1,4 @@
 
-"""
-sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git
-sudo pip install Theano
-
-sudo pip install keras
-
-sudo pip install cython
-sudo apt-get install libhdf5-dev
-sudo pip install h5py
-
-"""
 # Plot ad hoc CIFAR10 instances
 from keras.datasets import cifar10
 from matplotlib import pyplot
@@ -47,6 +36,13 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
+epochs = 250
+lrate = 0.01
+decay = lrate/epochs
+
+model = load_model('my_model.h5')
+
+"""
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, input_shape=(3, 32, 32), activation='relu', border_mode='same'))
 model.add(Dropout(0.2))
@@ -68,11 +64,9 @@ model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 # Compile model
-epochs = 250
-lrate = 0.01
-decay = lrate/epochs
 sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+"""
 print(model.summary())
 
 # Fit the model
@@ -85,10 +79,3 @@ print("Accuracy: %.2f%%" % (scores[1]*100))
 model.save('cifar10_model.h5')  # creates a HDF5 file 'my_model.h5'
 del model  # deletes the existing model
 
-# returns a compiled model
-# identical to the previous one
-"""
-model = load_model('my_model.h5')
-print model
-
-"""
